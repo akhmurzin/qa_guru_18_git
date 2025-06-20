8436b4d34638082aeeff6e4cbf6e53a8ec67522d
Adilzhan Akhmurzin <aakhmurzin0303@gmail.com>
2025-06-20 18:15:15 +0400

    Update Main.java

--- src/main/java/com/adil/Main.java
+++ src/main/java/com/adil/Main.java
@@ -3,9 +3,8 @@ package com.adil;
 public class Main {
 
     private static String msg = "Hello qa.guru!12312321";
-    
     public static void main(String[] args) {
-        System.out.printf(msg);
+        System.out.println(msg);
 
         for (int i = 1; i <= 3; i++) {
             System.out.println("i = " + i);
