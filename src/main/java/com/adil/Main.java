572d66f17158aac13bc966036fb904bc4a0f266e
Adilzhan Akhmurzin <adilzhan.akhmurzin@presight.ai>
2025-06-20 18:00:32 +0400

    added getMessage() method

--- src/main/java/com/adil/Main.java
+++ src/main/java/com/adil/Main.java
@@ -2,10 +2,14 @@ package com.adil;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello qa.guru!");
+        System.out.println(getMessage());
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+    }
+
+    private static String getMessage() {
+        return "Hello qa.guru!";
     }
 }
